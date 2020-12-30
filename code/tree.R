c2802a82b51680d1a4577ddf68551afbff1fcba4
MaxSjodin <max.lukas.sjodin@gmail.com>
2020-12-30 20:47:36 +0100

    Upd Rmd DecisionTree

--- code/tree.R
+++ code/tree.R
@@ -5,12 +5,14 @@ library(gbm)
 
 
 #Tree
-tree <- rpart(Occupancy~., data = trainingData, method = "class")
+formula <- Occupancy ~ .
+
+tree <- rpart(formula, data = trainingData, method = "class")
 rpart.plot(tree)
 treepred <- predict(tree, testingData, type = "class")
 table(testingData$Occupancy, treepred)
 
-formula <- Occupancy ~ .
+
 
 #Bagging
 bagged <- ipred::bagging(formula, data = trainingData, nbagg = 1000, coob = TRUE)
@@ -18,8 +20,6 @@ bagged <- ipred::bagging(formula, data = trainingData, nbagg = 1000, coob = TRUE
 print(bagged)
 
 bagpred <- predict(bagged, newdata = testingData)
-
-
 
 train.error <- mean(sqrt((as.numeric(as.character(bagpred)) - as.numeric(as.character(testingData$Occupancy)))^2))
 
