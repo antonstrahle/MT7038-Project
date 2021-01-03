761ab9e8f746643e26a978e60c2d261467281ece
antonstrahle <strahle@AntonDator.localdomain>
2021-01-03 16:13:49 +0100

    Added more on wknn

---
+++ code/wknn.R
@@ -0,0 +1,27 @@
+source("data.r")
+library(kknn)
+
+valWKNN <- function(K, kernel = "epanechnikov", train = trainingData, val = validationData){
+  
+  bestK <- 0
+  bestError <- 1
+  
+  for(k in K){
+    
+    m <- kknn(Occupancy ~ ., train = train, test = val, kernel = kernel, k = k) 
+    
+    valError <- mean(val[, sapply(val, is.factor)] != m$fitted.values)
+    
+    print(paste("Validation Error:", round(valError, digits = 3), "for k =", k))
+    
+    if(valError < bestError){
+      
+      bestK <- k
+      
+    }
+    
+  }
+  
+  bestK
+  
+}
